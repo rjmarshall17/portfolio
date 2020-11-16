3fc423c78ab8a601085df0221d85a3be1f94725c
Rob Marshall <rob.marshall17@gmail.com>
2020-11-15 21:37:57 -0500

    Final updates

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Job
+
+
+def home(request):
+    jobs = Job.objects
+    return render(request, 'jobs/home.html', {'jobs': jobs})
+
+
+def job_detail(request, job_id):
+    job = get_object_or_404(Job,pk=job_id)
+
+    return render(request, 'jobs/detail.html',{'job':job})
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',jobs.views.home,name='home'),
+    path('jobs/<int:job_id>',jobs.views.job_detail,name='job_detail')
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
